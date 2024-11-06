693e9c331f50f20fc60c3318541155e4ec8618f2
Sasha <154231066+jsenjoyer123@users.noreply.github.com>
2024-11-06 14:06:08 +0300

    add gradient

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -12,7 +12,8 @@ export default {
 
 <style>
 body {
-  background-color: blue;
+  background: rgb(2,0,36);
+background: linear-gradient(172deg, rgba(2,0,36,1) 0%, rgba(41,41,221,1) 0%, rgba(0,212,255,1) 100%);  
   margin: 0; 
 }
 
